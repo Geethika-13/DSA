19656ad3e7a78b6464988a8c5bb87f41dcc2eb9f
Geethika-13 <geethikab369@gmail.com>
2025-04-27 10:09:32 +0530

    Add files via upload

---
+++ linked list/pb2.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include<forward_list>
+#include<iterator>
+using namespace std;
+int main(){
+    forward_list<int> fl;
+    fl.push_front(1);
+    fl.push_front(3);
+    fl.push_front(5);
+    fl.push_front(7);
+    fl.push_front(9);
+    fl.push_front(11);
+    cout<<"The list contains the data entered:";
+    for(int x:fl)
+    cout<<x<<" ";
+    cout<<endl;
+    fl.reverse();
+    cout<<"The list after reversed:";
+    for(int x:fl)
+    cout<<x<<" ";
+
+    return 0;
+}

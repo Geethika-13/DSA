d7d6203422de72457298ce26c2ba78b0e272641e
Geethika-13 <geethikab369@gmail.com>
2025-04-29 20:05:46 +0530

    Add files via upload

---
+++ linked list/pb12.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include<forward_list>
+using namespace std;
+int main() {
+    forward_list<int>fl{1,2,3};
+    fl.emplace_front(6);
+    cout<<"The list:"<<endl;
+    for(int y:fl)
+    cout<<y<<" ";
+    cout<<endl;
+    int count = distance(fl.begin(),fl.end());
+    int midpos = count / 2;
+    auto itr = fl.begin();
+    for(int i = 0; i<midpos;i++){
+        itr++;
+    }
+    fl.insert_after(itr,5);//used to insert the element the after an element
+    cout<<"After inserting:"<<endl;
+    for(int x:fl)
+    cout<<x<<" ";
+    return 0;
+}
